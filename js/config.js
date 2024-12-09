0f7386dd703985bcad9fc41367a16f105d8e19c7
Pawel Dobrzanski <pawel.dobrzanski@hotmail.com>
2024-12-09 22:45:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_6w9p2ewDd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3292k180ml0frdjih724qju937', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
